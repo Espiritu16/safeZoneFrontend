333bd5d92b0ab16f59b3dc38ef2c9171229e922c
Kevin Espíritu <kevinespiritu16@gmail.com>
2026-05-28 23:28:40 -0500

    public: modulo inicio implementado

--- src/app/features/public/inicio/inicio.page.ts
+++ src/app/features/public/inicio/inicio.page.ts
@@ -1,1 +1,13 @@
-// Placeholder para pantalla publica de inicio.
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { PublicFooterComponent } from '../components/public-footer/public-footer.component';
+import { PublicHeaderComponent } from '../components/public-header/public-header.component';
+
+@Component({
+  selector: 'app-inicio',
+  standalone: true,
+  imports: [RouterLink, PublicHeaderComponent, PublicFooterComponent],
+  templateUrl: './inicio.page.html',
+  styleUrl: './inicio.page.css'
+})
+export class InicioPage {}
