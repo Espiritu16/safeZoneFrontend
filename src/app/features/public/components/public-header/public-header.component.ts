8109a8896e60ef99c9cb58bdf1bdf3bb7d00a2fb
Kevin Espíritu <kevinespiritu16@gmail.com>
2026-05-29 00:24:19 -0500

    modal login implementado

--- src/app/features/public/components/public-header/public-header.component.ts
+++ src/app/features/public/components/public-header/public-header.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LoginModalComponent } from '../login-modal/login-modal.component';
 
 @Component({
   selector: 'app-public-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, LoginModalComponent],
   templateUrl: './public-header.component.html',
   styleUrl: './public-header.component.css'
 })
-export class PublicHeaderComponent {}
+export class PublicHeaderComponent {
+  isLoginModalOpen = false;
+
+  openLoginModal(): void {
+    this.isLoginModalOpen = true;
+  }
+
+  closeLoginModal(): void {
+    this.isLoginModalOpen = false;
+  }
+}
