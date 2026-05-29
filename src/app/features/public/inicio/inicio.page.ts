3d479b8f7c1808bb0ce0b6524d634aade4d06143
Kevin Espíritu <kevinespiritu16@gmail.com>
2026-05-29 01:24:52 -0500

    public: correccion flujo login publico

--- src/app/features/public/inicio/inicio.page.ts
+++ src/app/features/public/inicio/inicio.page.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { PublicFooterComponent } from '../components/public-footer/public-footer.component';
 import { PublicHeaderComponent } from '../components/public-header/public-header.component';
+import { LoginModalComponent } from '../components/login-modal/login-modal.component';
 
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [RouterLink, PublicHeaderComponent, PublicFooterComponent],
+  imports: [PublicHeaderComponent, PublicFooterComponent, LoginModalComponent],
   templateUrl: './inicio.page.html',
   styleUrl: './inicio.page.css'
 })
-export class InicioPage {}
+export class InicioPage {
+  isLoginModalOpen = false;
+
+  openLoginModal(): void {
+    this.isLoginModalOpen = true;
+  }
+
+  closeLoginModal(): void {
+    this.isLoginModalOpen = false;
+  }
+}
