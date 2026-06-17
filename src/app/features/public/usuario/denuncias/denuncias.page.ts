1d6be20ad0ecbf2c6328d67fa3a55b44cd951d30
Kevin Espíritu <kevinespiritu16@gmail.com>
2026-06-16 21:03:25 -0500

    feat(victima): integrar panel de denuncias y consultas

--- src/app/features/public/usuario/denuncias/denuncias.page.ts
+++ src/app/features/public/usuario/denuncias/denuncias.page.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-usuario-denuncias-page',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './denuncias.page.html',
   styleUrl: './denuncias.page.css'
 })
