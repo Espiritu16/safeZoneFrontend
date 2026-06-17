1ec9e69669ddc97cde59bb0db13bd94afcfa047d
Kevin Espíritu <kevinespiritu16@gmail.com>
2026-06-16 21:03:30 -0500

    feat(predenuncias): integrar registro y formalizacion sprint 2

---
+++ src/app/core/services/predenuncias.service.ts
@@ -0,0 +1,33 @@
+import { Injectable, inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_ENDPOINTS } from '../http/api-endpoints';
+import { ApiClientService } from '../http/api-client.service';
+import type {
+  CrearPreDenunciaRequest,
+  EstadoPreDenuncia,
+  FormalizarPreDenunciaRequest,
+  PreDenunciaResponse,
+} from '../models/api.models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PredenunciasService {
+  private readonly api = inject(ApiClientService);
+
+  create(request: CrearPreDenunciaRequest): Observable<PreDenunciaResponse> {
+    return this.api.post<PreDenunciaResponse>(API_ENDPOINTS.predenuncias, request);
+  }
+
+  list(estado?: EstadoPreDenuncia): Observable<PreDenunciaResponse[]> {
+    return this.api.get<PreDenunciaResponse[]>(API_ENDPOINTS.predenuncias, { estado });
+  }
+
+  markInContact(id: string): Observable<PreDenunciaResponse> {
+    return this.api.patch<PreDenunciaResponse>(`${API_ENDPOINTS.predenuncias}/${id}/contactar`);
+  }
+
+  formalize(id: string, request: FormalizarPreDenunciaRequest): Observable<PreDenunciaResponse> {
+    return this.api.patch<PreDenunciaResponse>(`${API_ENDPOINTS.predenuncias}/${id}/formalizar`, request);
+  }
+}
