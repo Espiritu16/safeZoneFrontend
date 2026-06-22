2292157cbcfd88d48c36f698fd62a67fbcfe3cc1
Kevin Espíritu <kevinespiritu16@gmail.com>
2026-06-21 20:45:10 -0500

    fix(mobile): adapta api y viewport para ejecucion nativa

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:8080/api',
+  androidApiBaseUrl: 'http://10.0.2.2:8080/api',
+  iosApiBaseUrl: 'http://localhost:8080/api',
 };
